c8cd1a0aba188ee99a192d70c295cb319e47f9d9
suraj0-11 <surajtharun123@gmail.com>
2025-03-04 09:19:18 +0530

    hm

--- Queue_DSA.java
+++ Queue_DSA.java
@@ -3,13 +3,19 @@ import java.util.LinkedList;
 
 public class Queue_DSA {
     public static void main(String[] args) {
-        sur
+
         Queue<String> queue = new LinkedList<>();
         queue.offer("Minecraft");
         queue.offer("game");
         queue.offer("suraj");
         queue.offer("haha");  
+
+        // queue.poll();
+        // queue.poll();
+        // queue.poll();
+        // queue.poll();
         
         System.out.println(queue);
+        System.out.println(queue.contains("game"));
     }
 }
